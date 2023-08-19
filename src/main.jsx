94355c0a978de0a663c5e2840d2fac1011fa717e
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2023-08-19 10:03:09 +0530

    minor fix for header

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="header">
       <p className="live-poll-tracker">Live Poll Tracker</p>
-      <p className="credits">Hosted with ❤️ by Akash and Kishore</p>
+      <p className="credits">hosted with ❤️ by <a href="https://github.com/akashShanmugraj">@akashShanmugraj</a> and <a href="https://github.com/kishoreadhith-v/poll-results-live-tracker/">@kishoreadhith-v</a></p>
     </div>
     <App />
   </React.StrictMode>
