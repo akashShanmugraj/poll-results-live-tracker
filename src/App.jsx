d6969f7af5e308210803e95d073c3c10d2c81974
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2023-12-10 03:29:36 +0000

    ec2 latest

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,12 @@ function objecttoArray(data) {
   return dataArray;
 }
 
-const socket = io("ws://localhost:4040");
-socket.emit("nconn", "1zzB8xGjJcVpJrr-RpZsJU441-x6xcWE3V4SWXKifnpo");
+const socket = io("wss://socket.akashshanmugaraj.com");
+socket.emit("nconn", "1xRCUrDrctRtHAAxFyFowYNpB4pt3Nxuviw4_gm4-bQQ");
+
+const { timeZone } = Intl.DateTimeFormat().resolvedOptions();
+console.log(timeZone);
+
 socket.on("load", (data) => {
   console.log("DATALOAD");
   dataArray = objecttoArray(data);
@@ -50,12 +54,12 @@ export default function App() {
   return (
     <div className="main">
       <div className="info">
-        <h3 className="results">Results</h3>
+        <h3 className="results">Best Viewing Experience at 67% zoom</h3>
         <h1 className="sem-poll">Semester 3 CR Poll</h1>
         <p className="form-host-info">Form hosting by S Akash (22z255@psgtech.ac.in)</p>
-
-          <p className={isLive ? 'live-active':'live-inactive'} onClick={handleClick}>
-            <b>◉ {isLive ? 'LIVE':'CLOSED'}</b>
+        <p className="form-host-info">This form was closed on 21st September 2023, please wait till 10th December 2023 22:00 IST while I try to add interactiveness</p>
+          <p className='live-inactive'>
+            <b>◉ CLOSED</b>
           </p>
         </div>
       <div className="result-cards-container">{globalRenderCharts}</div>
