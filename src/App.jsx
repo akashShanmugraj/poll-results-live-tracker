6307af7a124fceabd425a6b97e3b3ef3c6383e95
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-06-20 17:47:48 +0530

    updated form for placement season

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function objecttoArray(data) {
 }
 
 const socket = io("wss://socket.akashshanmugaraj.com");
-socket.emit("nconn", "1TKKFDUWc4k10imc63t1GyQxOHhOiqhQDn-yVAeRQoPg");
+socket.emit("nconn", "1M2p0WgGQDTe-Pt8DO-8JK0nfgoRkr1apqiqKqsyzacQ");
 
 const { timeZone } = Intl.DateTimeFormat().resolvedOptions();
 console.log(timeZone);
@@ -81,8 +81,9 @@ export default function App() {
     <div className="main">
       <div className="info">
         <h3 className="results">Best Viewing Experience at 67% zoom</h3>
-        <h1 className={doAbort? "cheat-notice" : "sem-poll"}>{doAbort ? "🫵 tried to cheat in a CR Poll 😒" : "Semester 4 CR Poll"}</h1>
-        {/* <p className="form-host-info">Form hosting by S Akash (22z255@psgtech.ac.in)</p> */}
+        <h1 className={doAbort? "cheat-notice" : "sem-poll"}>{doAbort ? "🫵 tried to cheat in a CR Poll 😒" : "Live Poll Demonstration"}</h1>
+        <h3 className="results">Try this out live for yourself at <a href="https://forms.gle/fmciUA5iAzEttwXT6">here</a></h3>
+
         {/* <p className="form-host-info">This form was closed on 21st September 2023, please wait till 10th December 2023 22:00 IST while I try to add interactiveness</p> */}
         <p
           className={isLive ? "live-active" : "live-inactive"}
